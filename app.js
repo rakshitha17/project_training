3a1ee3875d3518498b2da01af03272b24a91c638
rakshitha17 <41372373+rakshitha17@users.noreply.github.com>
2018-07-21 08:15:52 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -12,9 +12,20 @@ function loadCustomer(e) {
         const profile = JSON.parse(this.responseText);
         
         document.getElementById('welcome').innerHTML=`<p>${profile.welcome}</p>`;
-        document.getElementById('customer').innerHTML=`<p>${profile.customer}</p>`;
+        // document.getElementById('customer').innerHTML=`<p>${profile.customer}</p>`;
       }
     }
   
     xhr.send();
   }
+
+  var header = document.getElementById("myDIV");
+var btns = header.getElementsByClassName("btn");
+for (var i = 0; i < btns.length; i++)
+ {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
