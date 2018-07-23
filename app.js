341ce362d32fd35e7fde37df17871af1b9747e3c
rakshitha17 <41372373+rakshitha17@users.noreply.github.com>
2018-07-23 07:13:36 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,28 +1,38 @@
 
 
 function loadCustomer(e) {
+  //alert("hai")
     const xhr = new XMLHttpRequest();
   
-    xhr.open('GET', '/json/index.json', true);
+    xhr.open('GET', '/json/index.json ', true);
   
     xhr.onload = function(){
       if(this.status === 200) {
-        // console.log(this.responseText);
+        console.log(this.responseText);
   
         const profile = JSON.parse(this.responseText);
         
-        document.getElementById('welcome').innerHTML=`<p>${profile.welcome}</p>`;
-         document.getElementById('customer').innerHTML=`<p>${profile.customer}</p>`;
+        document.getElementById('welcome').innerHTML=`<p>${profile.name}</p>`;
+        document.getElementById('customer').innerHTML=`<p>${profile.customer}</p>`;
       }
     }
-  
+    //alert("i am here")
     xhr.send();
   }
 
-  var header = document.getElementById("myDIV");
+
+  function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+
+var header = document.getElementById("myDIV");
 var btns = header.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++)
- {
+for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
     var current = document.getElementsByClassName("active");
     current[0].className = current[0].className.replace(" active", "");
