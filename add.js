ca04e2d5df5e3f4532e14ff64216202b98af8ec0
rakshitha17 <41372373+rakshitha17@users.noreply.github.com>
2018-07-23 11:52:11 -0700

    Add files via upload

---
+++ add.js
@@ -0,0 +1,10 @@
+function displayAddWidget()
+{
+    var widget = document.getElementById("widgetLayoutAdd");
+    widget.style.display="flex";
+}
+function removeAddWidget()
+{
+    var widget = document.getElementById("widgetLayoutAdd");
+    widget.style.display="none";
+}
